f25da9d3c4db5e92c5fd856875cc4d650d1b2d63
NguyenKDuy <nguyenkhacduy16112004@gmail.com>
2025-03-22 10:18:17 +0700

    Add folder RISC8b - have asm code to run in .sim/sim1/behav/xsim

---
+++ RISC8b/RISC8b.srcs/sim_1/new/tb_risc8b.sv
@@ -0,0 +1,46 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/20/2025 11:31:30 PM
+// Design Name: 
+// Module Name: tb_risc8b
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_risc8b#(
+        parameter DATA_WIDTH = 8,                  // Độ rộng dữ liệu
+        parameter ADDR_WIDTH = 5                  // Độ rộng địa chỉ (cho 2^5 = 32 ô)
+)(
+
+    );
+    logic clk;
+    logic rst;
+    logic [ADDR_WIDTH-1:0] pc_counter;
+    logic [DATA_WIDTH-1:0] alu_out;
+    logic halt;
+
+    risc_8b dut (clk, rst, pc_counter, alu_out, halt);
+    
+    initial begin
+        clk = 0;
+        rst = 1;
+        #35;
+        rst = 0;
+    end
+    always #10 clk = ~clk;
+    
+    
+endmodule
